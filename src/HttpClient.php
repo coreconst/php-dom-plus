48fdca1867d5ac7aac9a8055161b28f853d0d144
constantine <parfilokostya0@gmail.com>
2024-08-16 14:08:09 +0300

    Add httpclient

---
+++ src/HttpClient.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace PhpDomPlus;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+
+class HttpClient
+{
+    protected Client $client;
+
+    public function __construct(Client $client = null) {
+        $this->client = $client ?: new Client([
+            'headers' => [
+                'Content-Type' => 'text/html',
+            ],
+        ]);
+    }
+
+    /**
+     * @throws GuzzleException
+     */
+    public function getPage($url): string
+    {
+        $page = $this->client->get($url);
+        return $page->getBody();
+    }
+}
---
+++ tests/HttpClientTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace tests;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\RequestException;
+use GuzzleHttp\Psr7\Request;
+use GuzzleHttp\Psr7\Response;
+use PhpDomPlus\HttpClient;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class HttpClientTest extends TestCase
+{
+    /** @var HttpClient */
+    private $httpClient;
+
+    /** @var Client|MockObject */
+    private $clientMock;
+
+
+    protected function setUp(): void
+    {
+        $this->clientMock = $this->createMock(Client::class);
+        $this->httpClient = new HttpClient($this->clientMock);
+    }
+
+
+    public function testGetPageReturnsPageContent(): void
+    {
+        $url = 'http://example.com';
+        $expectedContent = '<html>...</html>';
+
+        $responseMock = new Response(200, [], $expectedContent);
+
+        $this->clientMock->method('get')
+            ->with($url)
+            ->willReturn($responseMock);
+
+        $content = $this->httpClient->getPage($url);
+
+        $this->assertEquals($expectedContent, $content);
+    }
+
+    public function testGetPageThrowsExceptionOnError(): void
+    {
+        $url = 'http://example.com';
+
+        $responseMock = new RequestException("Request failed", new Request('GET', $url));
+
+        $this->clientMock->method('get')
+            ->with($url)
+            ->willThrowException($responseMock);
+
+        $this->expectException(RequestException::class);
+
+        $this->httpClient->getPage($url);
+    }
+
+}
