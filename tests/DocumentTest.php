192c876878503f81a811c4c45406f7d0986aaf71
constantine <parfilokostya0@gmail.com>
2024-08-16 16:23:23 +0300

    Add DocumentTest

---
+++ tests/DocumentTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace tests;
+
+use PhpDomPlus\Document;
+use PHPUnit\Framework\TestCase;
+
+class DocumentTest  extends TestCase
+{
+    /** @var Document */
+    protected $document;
+
+    protected function setUp(): void
+    {
+        $this->document = new Document();
+        $this->document->loadHTML('<html><body><div id="test" class="example">content</div></body></html>');
+    }
+
+    public function testGetElementsByClassNameReturnsElement(): void
+    {
+        $element = $this->document->getElementsByClassName('example')->item(0);
+
+        $this->assertInstanceOf(\DOMNode::class, $element);
+        $this->assertEquals('test', $element->getAttribute('id'));
+    }
+
+    public function testGetElementsByClassNameReturnsNullForNoMatch(): void
+    {
+        $element = $this->document->getElementsByClassName('non-existent')->item(0);
+        $this->assertNull($element);
+    }
+
+}
