e3ec2acc8e315453b23bf8056f21dd8b31467235
constantine <parfilokostya0@gmail.com>
2024-08-20 14:37:57 +0300

    Improve loadHTMLByUrl

--- src/Document.php
+++ src/Document.php
@@ -2,6 +2,9 @@
 
 namespace PhpDomPlus;
 
+use Exception;
+use HTMLPurifier;
+use HTMLPurifier_Config;
 use PhpDomPlus\Service\HttpClient;
 use PhpDomPlus\Traits\HasElements;
 
@@ -9,7 +12,9 @@ class Document extends \DOMDocument
 {
     use HasElements;
 
-    protected ?string $url = null;
+    protected ?string $loadedPage = null;
+
+    protected ?Exception $exception = null;
 
     public function __construct() {
         parent::__construct();
@@ -19,14 +24,66 @@ class Document extends \DOMDocument
 
     }
 
-//    /**
-//     * @throws GuzzleException
-//     */
-//    public function loadHTMLByUrl($url): void
-//    {
-//        $client = new HttpClient();
-//        $page = $client->getPage($url);
-//        $this->loadHTML($page);
-//    }
+    /**
+     * Load html by link
+     *
+     * @param string $url
+     * @param bool $loadPurifiedHtmlWhileException <p>
+     *     If true, purifies HTML using HTMLPurifier when an exception occurs.
+     * </p>
+     * @param bool $ignoreLoadHtmlErrorsWhileException *Not recommended.** <p>
+     *     If true, suppresses errors and warnings during loadHTML.
+     * </p>
+     *
+     * @return void
+     * @throws Exception if both $loadPurifiedHtmlWhileException and $ignoreLoadHtmlErrorsWhileException are set to false.
+     */
+    public function loadHTMLByUrl(string $url,
+                                  bool $loadPurifiedHtmlWhileException = true,
+                                  bool $ignoreLoadHtmlErrorsWhileException = false): void
+    {
+        $client = new HttpClient();
+        $page = $client->getPage($url);
 
+        if(!isset($page)){
+            $this->loadedPage = null;
+            $this->exception = $client->getException();
+            return;
+        }
+
+        $this->loadedPage = $page;
+
+        try {
+            $this->loadHTML($page);
+            $this->exception = null;
+        } catch (Exception $e){
+
+            $this->exception = $e;
+
+            if($loadPurifiedHtmlWhileException){
+                $config = HTMLPurifier_Config::createDefault();
+                $purifier = new HTMLPurifier($config);
+                $cleanHtml = $purifier->purify($page);
+                $this->loadHTML($cleanHtml);
+                return;
+            }
+            if($ignoreLoadHtmlErrorsWhileException){
+                $page = mb_convert_encoding($page, 'HTML-ENTITIES', 'UTF-8');
+                @$this->loadHTML($page, LIBXML_NOERROR | LIBXML_NOWARNING);
+                return;
+            }
+
+            throw $e;
+        }
+    }
+
+    public function getLoadedPage(): ?string
+    {
+        return $this->loadedPage;
+    }
+
+    public function getException(): ?Exception
+    {
+        return $this->exception;
+    }
 }
